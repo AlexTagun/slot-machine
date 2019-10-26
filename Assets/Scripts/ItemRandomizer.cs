5dbbc2fa9593da343a04e2a7f297eca3bf736c17
alextagun <alextagun@gmail.com>
2019-10-27 00:04:59 +0300

    added ItemRandomizer and impotring it in prefabs

---
+++ Assets/Scripts/ItemRandomizer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemRandomizer : MonoBehaviour
+{
+    [SerializeField] List<Sprite> _itemImageList;
+
+    public Sprite getRandomSprite() {
+        return _itemImageList[Random.RandomRange(0, _itemImageList.Count)];
+    }
+}
--- Assets/Scripts/SpinItem.cs
+++ Assets/Scripts/SpinItem.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpinItem : MonoBehaviour {
     [SerializeField] private float _speed = 0;
 
     [SerializeField] private GameObject _topBorder;
     [SerializeField] private GameObject _bottomBorder;
+    [SerializeField] private ItemRandomizer _itemRandomizer;
 
     private RectTransform _rectTransform;
+    private Image _image; 
 
     private float _topY;
     private float _bottomY;
@@ -17,8 +20,10 @@ public class SpinItem : MonoBehaviour {
 
     private void Start() {
         _rectTransform = GetComponent<RectTransform>();
+        _image = GetComponent<Image>();
         _topY = _topBorder.transform.position.y;
         _bottomY = _bottomBorder.transform.position.y;
+        SetRandomSprite();
     }
 
     private void FixedUpdate() {
@@ -27,7 +32,16 @@ public class SpinItem : MonoBehaviour {
 
         if(transform.position.y <= _bottomY - _rectTransform.rect.height / 2) {
             transform.Translate(0, _topY - _bottomY + _rectTransform.rect.height, 0);
+            OnPositionReset();
         }
+    }
+
+    private void OnPositionReset() {
+        SetRandomSprite();
+    }
+
+    private void SetRandomSprite() {
+        _image.sprite = _itemRandomizer.getRandomSprite();
     }
 
     public void SetSpeed(float speed) {
