42ce3cb68377d56204bbe5692c76cefb1fd9b805
alextagun <alextagun@gmail.com>
2019-10-27 02:02:36 +0300

    added anchors to prefabs

---
+++ Assets/Scripts/ItemViewHeightPositionFixer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemViewHeightPositionFixer : MonoBehaviour {
+
+    private RectTransform _rectTransform;
+
+    private void Awake() {
+        _rectTransform = GetComponent<RectTransform>();
+        _rectTransform.offsetMax = new Vector2(_rectTransform.offsetMax.x, _rectTransform.rect.height / 3);
+        Debug.Log("done");
+    }
+
+}
