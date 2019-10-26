d075a75aad65d9b0240f69db1b45a1f9ce1b66cf
alextagun <alextagun@gmail.com>
2019-10-26 23:28:40 +0300

    added prefabs

--- Assets/Scripts/MainSpinItem.cs
+++ Assets/Scripts/MainSpinItem.cs
@@ -11,16 +11,11 @@ public class MainSpinItem : MonoBehaviour {
 
     private void Start() {
         _rectTransform = GetComponent<RectTransform>();
-        //Debug.Log(_rectTransform.localPosition.y);
         _startY = _rectTransform.localPosition.y;
-        //Debug.Log(_startY);
     }
     private void Update() {
-        //Debug.Log(_isNeedToStop);
-        //Debug.Log(Mathf.RoundToInt(_rectTransform.localPosition.y).Equals(Mathf.RoundToInt(_startY)));
-        if (_isNeedToStop && Mathf.RoundToInt(_rectTransform.localPosition.y).Equals(Mathf.RoundToInt(_startY))) {
-            //Debug.Log("Stop");
-            //Debug.Log(_rectTransform.localPosition.y);
+        if (_isNeedToStop && _rectTransform.localPosition.y.Equals(_startY)) {
+            Debug.Log("Stop");
             foreach (SpinItem item in _spinItems) {
                 item.StopMoving();
             }
@@ -34,7 +29,7 @@ public class MainSpinItem : MonoBehaviour {
     public void StartRotating() {
         _isNeedToStop = false;
         foreach (SpinItem item in _spinItems) {
-            item.StartMoving(300f);
+            item.StartMoving(1000f);
         }
     }
 }
